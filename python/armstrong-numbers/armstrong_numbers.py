11578904dc3eba27e65bd2e2640211ca9dc8bfcd
Shivam Shukla <shivam99aa@gmail.com>
2020-03-05 18:24:33 +0530

    sol2 for armstrong numbers

--- python/armstrong-numbers/armstrong_numbers.py
+++ python/armstrong-numbers/armstrong_numbers.py
@@ -1,13 +1,4 @@
 def is_armstrong_number(number):
-    if number < 10:
-        return True
+
     power = len(str(number))
-    sm = 0
-    test_number = number
-    while test_number != 0:
-        sm += pow(test_number % 10, power)
-        test_number = test_number // 10
-    if number != sm:
-        return False
-    else:
-        return True
+    return number == sum([int(i) ** power for i in str(number)])
