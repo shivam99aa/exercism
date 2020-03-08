c185be8722705299ec45ca766d92cc840b212a2f
Shivam Shukla <shivam99aa@gmail.com>
2020-03-08 17:24:31 +0530

    added sol for resistant color problem

---
+++ python/resistor-color/resistor_color.py
@@ -0,0 +1,7 @@
+color_code_list = ['black', 'brown', 'red', 'orange', 'yellow', 
+              'green', 'blue', 'violet', 'grey', 'white']
+def color_code(color):
+    return color_code_list.index(color)
+    
+def colors():
+    return color_code_list
